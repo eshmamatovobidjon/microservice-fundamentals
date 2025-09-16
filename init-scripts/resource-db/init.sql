3f0bbe59fd8732cb3fcefad57d539060042573d0
Obidjon_Eshmamatov <Obidjon.Eshmamatov@wolterskluwer.com>
2025-09-17 00:33:58 +0500

    containerization

--- init-scripts/resource-db/init.sql
+++ init-scripts/resource-db/init.sql
@@ -1,4 +1,6 @@
 CREATE TABLE IF NOT EXISTS resources (
     id SERIAL PRIMARY KEY,
-    data BIGINT NOT NULL
+    s3url VARCHAR(255),
+    original_file_name VARCHAR(255),
+    uploaded_at TIMESTAMP
 );
